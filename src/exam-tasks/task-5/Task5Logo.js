cce11339781ecfc1da865ec6704b0a6b39ff3c50
194pp <zilvinas111@gmail.com>
2021-11-22 11:04:46 +0200

    Task5 refactored.

--- src/exam-tasks/task-5/Task5Logo.js
+++ src/exam-tasks/task-5/Task5Logo.js
@@ -2,7 +2,7 @@ import classes from "./Task5.module.css";
 
 const Task5Logo = () => {
   return (
-    <img alt='logo' className={classes.Logo} src='https://www.europanostra.org/wp-content/uploads/2017/09/2017-09-Twitter-logo.png'/>
+    <i className={`fab fa-twitter ${classes.Logo}`}/>
   )
 }
 
